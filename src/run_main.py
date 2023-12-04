1cb9ba8a706633305f7a64552cdba6d1abd244b0
Victor Hernandez <victor.993_@hotmail.com>
2023-12-04 07:28:24 -0500

    PNN-6 Objetive implemented

--- src/run_main.py
+++ src/run_main.py
@@ -1,7 +1,7 @@
 import os
 from database import PostgresConnection
 from extract_data import ExtractData
-from transform_data import GuidelineT, ProductT, InstitutionT, MilestoneT
+from transform_data import GuidelineT, ProductT, InstitutionT, MilestoneT, ObjectiveT
 from load import LoadData
 
 class ETLMaster():
@@ -60,6 +60,9 @@ class ETLMaster():
             milestone = MilestoneT(data, load)
             milestone.run_milestone()
 
+            objective = ObjectiveT(data, load)
+            objective.run_objective()
+            
         self.connection.disconnect()
         print("Proceso ETL completado con éxito.")
 
