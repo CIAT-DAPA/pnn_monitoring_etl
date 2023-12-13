00aa313ddf3e846dc8888e63a36caac1ecaafe14
santiagoC <s.calderon@cgiar.org>
2023-12-13 09:18:15 -0500

    fixed csv

--- src/rollback/rollback.py
+++ src/rollback/rollback.py
@@ -5,6 +5,7 @@ from pnn_monitoring_orm import *
 import numpy as np
 from sqlalchemy.orm import class_mapper
 from tools import Tools
+import csv
 from datetime import datetime
 
 
@@ -20,10 +21,14 @@ class Rollback():
         self.config_file = os.path.join(self.config_path, "config_file.csv")
         self.folder_path = os.path.join(self.output_path, id)
         self.error_log_file = "rb_log.txt"
-
+        self.csv_file_error = f"_deleted.csv"
+        
         self.actu_date = datetime.now()
+        format_data = self.actu_date.strftime("%Y%m%d_%H%M%S")
         
         self.tools = Tools(root_dir, self.actu_date)
+
+        self.output_folder = os.path.join(self.output_path, format_data)
 
         self.tools.check_folders([(self.folder_path, True), 
                                   (self.config_path, True), 
@@ -32,6 +37,7 @@ class Rollback():
         self.tools.check_files([self.config_file])
 
         os.makedirs(self.log_path, exist_ok=True)
+        os.makedirs(self.output_folder, exist_ok=True)
 
         self.encoding = "ISO-8859-1"
 
@@ -87,21 +93,26 @@ class Rollback():
 
                 dfs = self.read_csvs(element)
 
-                ids = np.concatenate([df["id"].to_numpy() for df in dfs]).tolist()
+                if len(dfs) > 0:
 
-                model = None
+                    ids = np.concatenate([df["id"].to_numpy() for df in dfs]).tolist()
 
-                for nombre, objeto in globals().items():
-                    if isinstance(objeto, type) and issubclass(objeto, Base) and objeto != Base and nombre == element:
-                        model = objeto
+                    model = None
 
-                print(f"\n\n---------------------  Borrando datos de la tabla: {element} -------------------------")
-                session.query(model).filter(model.id.in_(ids)).delete(synchronize_session=False)
+                    for nombre, objeto in globals().items():
+                        if isinstance(objeto, type) and issubclass(objeto, Base) and objeto != Base and nombre == element:
+                            model = objeto
 
-                session.commit()
+                    print(f"\n\n---------------------  Borrando datos de la tabla: {element} -------------------------\n")
+                    session.query(model).filter(model.id.in_(ids)).delete(synchronize_session=False)
 
-                msg = "Se han eliminado los registros con los siguientes IDs: \n{}".format(", ".join(map(str, ids)))
-                print(msg)
+                    session.commit()
+
+                    msg = f"Se eliminaron {len(ids)} registros de la base de datos."
+                    print(msg)
+                    print(f"\n\n-------------------------- ------------------------------")
+
+                    self.generate_csv_with_deleted(ids, element)
 
         except Exception as e:
             msg_error = f'Error al realizar el rollback: {str(e)}'
@@ -109,3 +120,29 @@ class Rollback():
             print(msg_error)
             return False
         
+
+
+    def generate_csv_with_deleted(self, data, tittle):
+
+        try:
+
+            file_name = f"{tittle}{self.csv_file_error}"
+
+            mode = "a" if os.path.isfile(os.path.join(self.output_folder,file_name)) else "w"
+
+
+            with open(os.path.join(self.output_folder,file_name), mode=mode, newline='') as file:
+                writer = csv.writer(file, delimiter=',', lineterminator='\n')
+                
+
+                if mode == 'w':
+                    writer.writerow(["ID"])
+
+
+                    
+                for id in data : writer.writerow ([id])
+        
+        except Exception as e:
+            msg_error = f"Error al generar el csv de errores: {str(e)}\n"
+            self.tools.write_log(msg_error, self.error_log_file)
+            print(msg_error)
